fb28363c226af5a20b35bae8db8d66ce5c643125
Ian Barrick <ianbarrick@gmail.com>
2016-11-15 17:45:10 -0500

    removed cache

--- addon/components/paper-virtual-repeat.js
+++ addon/components/paper-virtual-repeat.js
@@ -48,7 +48,6 @@ const VirtualRepeatComponent = VirtualEachComponent.extend({
     }
     return this.get('size');
   }),
-
   // Received coordinates {top, left, right, width} from the dropdown
   // Convert them to style and cache - they usually don't change
   positionStyle: computed('positionCoordinates', function() {
